8c59f89a185043a258c1dcfb23f61651f7e9279b
{arsapoz} <{arsaaa@live.com}>
2018-01-28 15:40:48 +0100

    nesto

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
+// budget controller
 var budgetController = (function(){
 
-	var x = 23;
 
-	var add = function(a){
-		return x + a;
+})();
+
+//ui controller
+var UIController = (function() {
+
+	 
+
+})();
+
+// global app controller
+var controller = (function(budgetCtrl, UICtrl){
+
+	var ctrlAddItem = function () {
+				// 1. get the field input data
+
+
+		// 2. add the item to the budget controller
+
+
+		// 3. add the item to the UI
+
+		// 4. calculate the budget
+
+
+		// 5. display the budget on the UI
+		console.log('itworks.');
 	}
 
-	return {
-		publicTest: function(b) {
-			console.log(add(b));
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event){
+
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
 		}
-	}
-})();
 
-var UIController = ((){
+	});
 
-	
-
-})();
+})(budgetController, UIController);
